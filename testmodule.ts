a85ade4043aeb79f2025320fc0ed6bad2fb1ecba
Fab55555 <127385093+Fab55555@users.noreply.github.com>
2023-06-20 23:03:22 -0500

    Create testmodule.ts

---
+++ testmodule.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+export class AppModule { }
